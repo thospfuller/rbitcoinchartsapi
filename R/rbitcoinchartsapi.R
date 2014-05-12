e01307cb2320faac78733f00a773286ec7e9eb0c
thospfuller <thospfuller@bitbucket.org>
2014-05-11 20:59:06 -0400

    Changed the version number to 1.0.0 and added some documentation.

--- R/rbitcoinchartsapi.R
+++ R/rbitcoinchartsapi.R
@@ -9,6 +9,15 @@
 .onUnload <- function (libpath) {
 }
 
+### Bitcoincharts offers weighted prices for several currencies at
+### http://api.bitcoincharts.com/v1/weighted_prices.json. You can use this to
+### price goods and services in Bitcoins. This will yield much lower
+### fluctuations than using a single market's latest price.
+### Weighted prices are calculated for the last 24 hours, 7 days and 30 days.
+### If there are no trades during an interval (like no trade within 24 hours)
+### no value will be returned.
+###
+### For example:
 ###
 ### weightedPrices <- GetWeightedPrices ()
 ###
@@ -18,6 +27,11 @@ GetWeightedPrices <- function () {
     return (dataFrame)
 }
 
+### You can access general market data at
+### http://api.bitcoincharts.com/v1/markets.json. This will return an array
+### with elements for each market.
+###
+### For example:
 ###
 ### params <- list (currency="USD")
 ###
@@ -29,6 +43,10 @@ GetMarketData <- function (params) {
     return (dataFrame)
 }
 
+### Trade data is available as CSV, delayed by approx. 15 minutes. It will
+### return the 2000 most recent trades.
+###
+### For example:
 ###
 ### params <- list (symbol="btceUSD")
 ###
@@ -42,13 +60,7 @@ GetMarketData <- function (params) {
 ### empty data frame.
 ###
 GetHistoricTradeData <- function (params) {
-    ###
-    ### http://api.bitcoincharts.com/v1/trades.csv?symbol=btceUSD
-    ###
-    ### params <- list (symbol="btceUSD")
-    ###
-    ### WORKS: read.csv (url ("http://api.bitcoincharts.com/v1/trades.csv?symbol=btceUSD"))
-    ###
+
     data <- getForm("http://api.bitcoincharts.com/v1/trades.csv", .params=params)
 
     tempCsvFile <- tempfile(pattern = "historicTradeData", tmpdir = tempdir(), fileext = ".csv")
